d10d59e09175f341247af6b807c51e6775435b66
Noura <Nouramalsbuayt@gmail.com>
2026-02-12 14:41:35 +0300

    ----

--- burnout/burnout/burnoutApp.swift
+++ burnout/burnout/burnoutApp.swift
@@ -1,19 +1,17 @@
-//
-//  burnoutApp.swift
-//  burnout
-//
-//  Created by Noura Alsbuayt on 14/08/1447 AH.
-//
-
 import SwiftUI
 import SwiftData
 
 @main
 struct burnoutApp: App {
+
+    init() {
+        UIView.appearance().semanticContentAttribute = .unspecified
+    }
+
     var body: some Scene {
         WindowGroup {
             SplashView()
         }
-        .modelContainer(for: [QuestionResponse.self, DailyRiskScore.self])
+        .modelContainer(for: [QuestionResponse.self])
     }
 }
